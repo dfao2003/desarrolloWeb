47ebe9e2086b81e9026ff4e11be4a69afbc02d72
mtoledot1 <mtoledot1@est.ups.edu.ec>
2023-10-17 21:54:00 -0500

    Cambio en el formulario, se puede añadir mas ingredientes

--- javascript/interaccion.js
+++ javascript/interaccion.js
@@ -1,35 +1,44 @@
 
-class Receta = {
-	var nombre;
-	var ingredientes = new Map();
-	var pasos = new Array():
+class Receta {
+	nombre = "";
+	ingredientes = new Map();
+	pasos = "";
 
-	constructor(){
-	}
-	
-	constructor(nombre, ingredientes, pasos){
-		this.nombre = nombre;
-		this.ingredientes = ingredientes;
-		this.pasos = pasos;
-	}
-	constructor(nombre){
-		this.nombre = nombre;
+	constructor(nom){
+		this.nombre = nom;
 	}
 }
 
-recetaNueva = class Receta();
 
-function crearReceta(nombre, ingredientes, pasos) {
-	recetaNueva.nombre = nombre;
-	recetaNueva.ingredientes = ingredientes;
+function crearReceta(nombre, pasos) {
+	recetaNueva = new Receta(nombre);
+	//recetaNueva.ingredientes = ingredientes;
 	recetaNueva.pasos = pasos;
 	
-	const p1 = document.createElement("p");
-	const nom = document.createTextNode(nombre);
-	p1.append(nom);
-	document.appendChild(p1);
-
-	//localStorage.setItem(recetaNueva);
 }
 
+function agregarIngrediente(){
+	const nom = document.createElement("label");
+	const nomval = document.createTextNode("Nombre");
+	const field1 = document.createElement("input");
+	const att = document.createAttribute("type");
+	att.value = "text";
+	field1.setAttributeNode(att);
+	const cant = document.createElement("label");
+	const cantval = document.createTextNode("Cantidad");
+	const field2 = document.createElement("input");
+	const att2 = document.createAttribute("type");
+	att2.value = "text";
+	field2.setAttributeNode(att2);
+	const ingredientes = document.getElementById("ingredientes");
+	const boton = document.getElementById("addIng");
+	const br = document.createElement("br");
+	nom.append(nomval);
+	cant.append(cantval);
+	ingredientes.insertBefore(nom,boton);
+	ingredientes.insertBefore(field1,boton);
+	ingredientes.insertBefore(cant,boton);
+	ingredientes.insertBefore(field2,boton);
+	ingredientes.insertBefore(br,boton);
+}
 
