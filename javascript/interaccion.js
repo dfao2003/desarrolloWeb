7ea555d4bd4a212a77727ea8ac567befc219a056
mtoledot1 <mtoledot1@est.ups.edu.ec>
2023-10-18 07:21:15 -0500

    Se agrega el guardado en el localStorage de la receta y se hacen pruebas para la recuperacion de los datos guardados

--- javascript/interaccion.js
+++ javascript/interaccion.js
@@ -10,11 +10,60 @@ class Receta {
 }
 
 
+function replacerMap(key, value) {
+
+  	if(value instanceof Map) {
+    		return {
+      			dataType: 'Map',
+      			value: Array.from(value.entries()), // or with spread: value: [...value]
+   	 	};
+  	} else {
+    		return value;
+  	}
+}
+
+function reviverMap(key, value) {
+  	if(typeof value === 'object' && value !== null) {
+ 		if (value.dataType === 'Map') {
+      			return new Map(value.value);
+    		}
+  	}
+  	return value;
+}
+
 function crearReceta(nombre, pasos) {
 	recetaNueva = new Receta(nombre);
-	//recetaNueva.ingredientes = ingredientes;
 	recetaNueva.pasos = pasos;
 	
+	const lista = document.getElementById("ingredientes");
+	const ingredientes = lista.getElementsByTagName("input");
+	var i;
+	for (i = 0; i < ingredientes.length; i+=2) {
+ 		const ing = ingredientes[i].value;
+		const cant = ingredientes[i+1].value;
+		recetaNueva.ingredientes.set(ing, cant);
+	}
+	console.log(recetaNueva);
+	const receta = JSON.stringify(recetaNueva,replacerMap);
+	localStorage.setItem(nombre, receta);
+	var str = localStorage.getItem(nombre);
+	console.log(str);
+	const nuevo = JSON.parse(str, reviverMap);
+	console.log(nuevo);
+	str = localStorage.getItem("recetas");
+	console.log(str);
+	if(str != null){
+		recetas = JSON.parse(str);
+		if(str.includes(nombre)){
+			console.log("Ya existe una receta con ese nombre");
+			return;
+		}
+		recetas.push(nombre);
+	}else{
+		recetas = [nombre];
+	}
+	str = JSON.stringify(recetas);
+	localStorage.setItem("recetas",str);
 }
 
 function agregarIngrediente(){
